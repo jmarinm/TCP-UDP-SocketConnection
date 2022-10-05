4f564b40b186d87a94f2c66d88c2ea19ae7f2f95
jmarinm <j.marinm@uniandes.edu.co>
2022-10-05 02:56:33 -0500

    updated server

--- ServidorTCP/TCPServer.py
+++ ServidorTCP/TCPServer.py
@@ -1,24 +1,31 @@
-from email import message
-from http import client
 import socket
+import threading
 
-#Se especifíca el protocolo SOCK_STREAM = TCP
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-host = socket.gethostbyname("localhost")
+ 
+
+host = '0.0.0.0'
 port = 444
 
-serversocket.bind(('',port))
 
-serversocket.listen(25)
+def main():
+    #Se especifíca el protocolo SOCK_STREAM = TCP
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((host,port))
+    serversocket.listen(25)
+    print(f'[*] Listening on {host}:{port}')
 
-while True:
-    clientsocket, address = serversocket.accept()
+    while True:
+        client, address = serversocket.accept()
+        print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+        client_handler = threading.Thread(target=handle_client,args=(client,))
+        client_handler.start()
 
-    print("Received connection from {}".format(str(address)))
+def handle_client(client_socket):
+    with client_socket as sock:
+        request = sock.recv(1024)
+        print(f'[*] Received: {request.decode("utf-8")}')
+        sock.send(b'ACK')
 
-    msg = "Thank you for connecting to the server\n"
-
-    clientsocket.send(msg.encode('ascii'))
-
-    clientsocket.close()
+if __name__ == "__main__":
+    main()
