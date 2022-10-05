2a270e6a2a28229e6b0483f66b0e3fe9611c9815
jmarinm <j.marinm@uniandes.edu.co>
2022-10-05 02:21:36 -0500

    Update

--- ClienteTCP/TCPClient.py
+++ ClienteTCP/TCPClient.py
@@ -6,7 +6,7 @@ clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 444
 
-clientsocket.connect(('192.168.60.128'.port))
+clientsocket.connect(('192.168.60.128',port))
 
 msg = clientsocket.recv(1024)
 
